5ed913b0f451ac56b0763353b2f00e6c0bef60b2
ncskier <bcw58@cornell.edu>
2016-07-21 10:13:15 -0400

    Activity Checklist TableView Working

---
+++ completionist/Activity.swift
@@ -0,0 +1,13 @@
+//
+//  Activity.swift
+//  completionist
+//
+//  Created by Brandon Walker on 7/20/16.
+//  Copyright © 2016 Brandon Walker. All rights reserved.
+//
+
+import UIKit
+
+class Activity: NSObject {
+
+}
---
+++ completionist/ActivityCheckboxTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityCheckboxTableViewCell.swift
+//  completionist
+//
+//  Created by Brandon Walker on 7/20/16.
+//  Copyright © 2016 Brandon Walker. All rights reserved.
+//
+
+import UIKit
+
+class ActivityCheckboxTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
