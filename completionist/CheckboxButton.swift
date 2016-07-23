bf3b7279b4aecb4b7fa997f9eff1ca3b735b3e45
ncskier <bcw58@cornell.edu>
2016-07-23 12:05:24 -0400

    working list and completion iteration

---
+++ completionist/CheckboxButton.swift
@@ -0,0 +1,21 @@
+//
+//  CheckboxButton.swift
+//  completionist
+//
+//  Created by Brandon Walker on 7/21/16.
+//  Copyright © 2016 Brandon Walker. All rights reserved.
+//
+
+import UIKit
+
+class CheckboxButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ completionist/RequirementsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RequirementsTableViewCell.swift
+//  completionist
+//
+//  Created by Brandon Walker on 7/21/16.
+//  Copyright © 2016 Brandon Walker. All rights reserved.
+//
+
+import UIKit
+
+class RequirementsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
