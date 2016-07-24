3c450a85251378d248efc10a4bbf5815966cedf4
ncskier <bcw58@cornell.edu>
2016-07-24 00:57:17 -0400

    Mostly functional withour completion bars

---
+++ completionist/CompletedToGoalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CompletedToGoalTableViewCell.swift
+//  completionist
+//
+//  Created by Brandon Walker on 7/24/16.
+//  Copyright © 2016 Brandon Walker. All rights reserved.
+//
+
+import UIKit
+
+class CompletedToGoalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ completionist/DeleteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DeleteTableViewCell.swift
+//  completionist
+//
+//  Created by Brandon Walker on 7/23/16.
+//  Copyright © 2016 Brandon Walker. All rights reserved.
+//
+
+import UIKit
+
+class DeleteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
