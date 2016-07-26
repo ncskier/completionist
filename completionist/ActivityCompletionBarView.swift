75f37d7beded95a469c829fda2a8114bd88aeb2f
ncskier <bcw58@cornell.edu>
2016-07-25 20:00:54 -0400

    Basic application working (without dates)

---
+++ completionist/ActivityCompletionBarView.swift
@@ -0,0 +1,21 @@
+//
+//  ActivityCompletionBarView.swift
+//  completionist
+//
+//  Created by Brandon Walker on 7/25/16.
+//  Copyright © 2016 Brandon Walker. All rights reserved.
+//
+
+import UIKit
+
+class ActivityCompletionBarView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
